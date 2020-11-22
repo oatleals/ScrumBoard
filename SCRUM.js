3a8eff94fa11d557f29f728f0806817ac04eee13
Leala A. Othman <leala.othman@slu.edu>
2020-11-22 09:25:20 -0600

    Update SCRUM.js

--- SCRUM.js
+++ SCRUM.js
@@ -3,8 +3,8 @@ const addBtn = document.getElementById("add");
 const notes = JSON.parse(localStorage.getItem("notes"));
 
 if (notes) {
-    notes.forEach((note) => {
-        addNewNote(note);
+    notes.forEach(item => {
+        addNewNote(item.color, item.note);
     });
 }
 
@@ -12,13 +12,14 @@ addBtn.addEventListener("click", () => {
     addNewNote();
 });
 
-function addNewNote(text = "") {
+function addNewNote(color = "rgb(100, 149, 237)", text = "") {
     const note = document.createElement("div");
     note.classList.add("note");
 
     note.innerHTML = `
         <div class="notes">
-            <div class="tools">
+            <div class="tools" style="background-color:`+ color + `">
+                <select class="status"></select>
                 <button class="edit">EDIT</i></button>
                 <button class="delete">DELETE</i></button>
             </div>
@@ -26,6 +27,27 @@ function addNewNote(text = "") {
             <textarea class="${text ? "hidden" : ""}"></textarea>
         </div>
     `;
+
+    const statusSelect = note.querySelector(".status");
+    const toolsDiv = note.querySelector(".tools");
+
+    const obj = {
+        "rgb(100, 149, 237)": "Blue/新建",
+        "rgb(127, 255, 170)": "Mint/UI开发中",
+        "rgb(148, 0, 211)": "Purple/API开发中",
+        "rgb(255, 69, 0)": "Red/UI集成API",
+        "rgb(255, 192, 203)": "Pink/完成",
+    }
+
+    let str = "";
+    Object.entries(obj).map(([key, value]) => {
+        if (color === key) {
+            str += `<option value="` + key + `" selected>` + value + `</option>`;
+        } else {
+            str += `<option value="` + key + `">` + value + `</option>`;
+        }
+    })
+    statusSelect.innerHTML = str;
 
     const editBtn = note.querySelector(".edit");
     const deleteBtn = note.querySelector(".delete");
@@ -35,6 +57,12 @@ function addNewNote(text = "") {
 
     textArea.value = text;
     main.innerHTML = marked(text);
+
+    statusSelect.addEventListener("change", e => {
+        toolsDiv.style.backgroundColor = e.target.value;
+
+        updateLS();
+    });
 
     editBtn.addEventListener("click", () => {
         main.classList.toggle("hidden");
@@ -59,12 +87,21 @@ function addNewNote(text = "") {
 }
 
 function updateLS() {
+    const toolsArr = document.querySelectorAll(".tools");
+    const toolsColorArr = [];
+    toolsArr.forEach(item => { toolsColorArr.push(item.style.backgroundColor) });
+
     const notesText = document.querySelectorAll("textarea");
+    const notesTextArr = [];
+    notesText.forEach(item => { notesTextArr.push(item.value) });
 
     const notes = [];
 
-    notesText.forEach((note) => {
-        notes.push(note.value);
+    notesText.forEach((note, index) => {
+        notes.push({
+            color: toolsColorArr[index],
+            note: note.value,
+        });
     });
 
     localStorage.setItem("notes", JSON.stringify(notes));
